c67b1dbf5bd844476170ba19610d0442c83509c9
arkayhani <arkayhani@gmail.com>
2015-12-17 03:47:17 +0330

    new

---
+++ NFA2DFA/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace NFA2DFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(93, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(267, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 134);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(77, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start (s0 will always be your start):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alphabet (insert a-z or landa):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "End:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(625, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 44);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(575, 82);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(172, 186);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(131, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Number of alphabet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(264, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "NFA:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(572, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "DFA:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 343);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ NFA2DFA/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NFA2DFA
+{
+    public partial class Form1 : Form
+    {
+
+        int alphanum = 10;
+        List<link> nfa = new List<link>();
+        List<link> newnfa = new List<link>();
+        List<link> dfa = new List<link>();
+        class link
+        {
+            public string start;
+            public string end;
+            public string aplha;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            link temp = new link();
+            temp.start = textBox1.Text;
+            temp.aplha = textBox2.Text;
+            temp.end = textBox3.Text;
+            nfa.Add(temp);
+
+            listBox1.Items.Add(textBox1.Text + " ===" + textBox2.Text + "===> " + textBox3.Text);
+            textBox1.Text="";
+            textBox2.Text="";
+            textBox3.Text="";
+            button2.Enabled = true;
+          
+        }
+        string landaend(string start)
+        {
+            string resualt = "";
+            foreach (var item in nfa)
+            {
+                if (item.start == start && item.aplha == "landa")
+                {
+                    resualt += item.end + landaend(item.end);
+                }
+            }
+            return resualt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            link tempn = new link();
+            tempn.start = "s0"+landaend("s0");
+            newnfa.Add(tempn);
+           
+            while (newnfa.Count > 0)
+            {
+                foreach (var item in newnfa)
+                {
+                    for (int i = 0; i < alphanum; i++)
+                    {
+                        string alphatemp = (Convert.ToChar('a' + i)).ToString();
+                        link temp = new link();
+                        string end = "";
+                        for (int j = 0; j < item.start.Length; j+=2)
+                        {
+                            string st = item.start.Substring(j, 2);
+                            foreach (var item2 in nfa)
+                            {
+                                if (st == item2.start && alphatemp == item2.aplha)
+                                {
+                                    end += item2.end + landaend(item2.end);
+                                }
+                            }
+                        } if (end != "")
+                        {
+                            temp.start = item.start;
+                            temp.aplha = alphatemp;
+                            temp.end = end;
+                            dfa.Add(temp);
+                            Boolean flag = true;
+                            foreach (var i3 in dfa)
+                            {
+                                if (i3.start == item.start)
+                                    flag = false;
+                            }
+                            if (flag)
+                            {
+                                link tempnf = new link();
+                                tempnf.start = item.start;
+                                newnfa.Add(tempnf);
+                            }
+                            
+                        }
+
+
+                    }
+                    newnfa.Remove(item);
+                    break;
+                }
+            }
+            foreach (var item in dfa)
+            {
+                listBox2.Items.Add(item.start + " ===" + item.aplha + "===> " + item.end);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            alphanum = Convert.ToInt32(textBox4.Text);
+            button1.Enabled = true;
+            button3.Enabled = false;
+        }
+    }
+}
